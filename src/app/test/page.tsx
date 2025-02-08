d0c8fbf2b3c51b7d18affda583bd369b929a2a32
Juiiceee <lbehr@student.42.fr>
2025-02-08 17:12:17 +0000

    fonctionne pour usdc vers avax

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,202 +1,190 @@
 "use client";
+import { useState } from "react";
+import { getTokenAvax, getBaseAvax } from "@/constants/tokenInfo";
 import {
-  ChainId,
-  WNATIVE,
-  Token,
-  TokenAmount,
-  Percent,
+	ChainId,
+	Token,
+	TokenAmount,
+	Percent,
 } from "@traderjoe-xyz/sdk-core";
 import {
-  PairV2,
-  RouteV2,
-  TradeV2,
-  TradeOptions,
-  LB_ROUTER_V22_ADDRESS,
-  jsonAbis,
+	PairV2,
+	RouteV2,
+	TradeV2,
+	TradeOptions,
+	LB_ROUTER_V22_ADDRESS,
+	jsonAbis,
 } from "@traderjoe-xyz/sdk-v2";
 import {
-  createPublicClient,
-  createWalletClient,
-  http,
-  parseUnits,
-  BaseError,
-  ContractFunctionRevertedError,
+	createPublicClient,
+	http,
+	parseUnits,
+	Address
 } from "viem";
 import { avalancheFuji } from "viem/chains";
-import { useBalance, useAccount, useWriteContract } from "wagmi";
+import { useAccount, useWriteContract, useBalance } from "wagmi";
+import { abiApprouve } from "@/constants/abi";
 
 export default function SwapComponent() {
-  const { address } = useAccount();
-  const { LBRouterV22ABI } = jsonAbis;
-  const CHAIN_ID = 43113;
-  const router = LB_ROUTER_V22_ADDRESS[CHAIN_ID];
-  const WAVAX = WNATIVE[CHAIN_ID];
-  const USDC = new Token(
-    CHAIN_ID,
-    "0xB6076C93701D6a07266c31066B298AeC6dd65c2d",
-    6,
-    "USDC",
-    "USD Coin"
-  );
+	const [selectedOption1, setSelectedOption1] = useState<string>('USDC');
+	const [selectedOption2, setSelectedOption2] = useState<string>('WAVAX');
+	const [amount, setAmount] = useState<string>("0");
+	const { address, chainId } = useAccount();
+	const { LBRouterV22ABI } = jsonAbis;
+	const CHAIN_ID = 43113;
+	const router = LB_ROUTER_V22_ADDRESS[CHAIN_ID];
+	const BASES = [getTokenAvax("WAVAX", CHAIN_ID), getTokenAvax("USDC", CHAIN_ID), getTokenAvax("USDT", CHAIN_ID)];
+	const inputToken = getTokenAvax(selectedOption1, CHAIN_ID);
+	const outputToken = getTokenAvax(selectedOption2, CHAIN_ID);
+	const isExactIn = true;
+	const typedValueIn = amount;
+	const typedValueInParsed = parseUnits(typedValueIn, inputToken.decimals);
+	const amountIn = new TokenAmount(inputToken, typedValueInParsed);
 
-  const USDT = new Token(
-    CHAIN_ID,
-    "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7",
-    6,
-    "USDT",
-    "Tether USD"
-  );
+	const allTokenPairs = PairV2.createAllTokenPairs(
+		inputToken,
+		outputToken,
+		BASES
+	);
 
-  const BASES = [WAVAX, USDC, USDT];
-  const inputToken = USDC;
-  const outputToken = WAVAX;
-  const isExactIn = true;
-  const typedValueIn = "20";
-  const typedValueInParsed = parseUnits(typedValueIn, inputToken.decimals);
-  const amountIn = new TokenAmount(inputToken, typedValueInParsed);
+	const {
+		data: approvalHash,
+		writeContract: writeApproval,
+		error: approvalError,
+		isSuccess: isApprovalSuccess,
+	} = useWriteContract();
 
-  const allTokenPairs = PairV2.createAllTokenPairs(
-    inputToken,
-    outputToken,
-    BASES
-  );
+	const {
+		data: swapHash,
+		writeContract: writeSwap,
+		error: swapError
+	} = useWriteContract();
 
-  const { 
-    data: approvalHash, 
-    writeContract: writeApproval, 
-    error: approvalError,
-    isSuccess: isApprovalSuccess,
-  } = useWriteContract();
+	const allPairs = PairV2.initPairs(allTokenPairs);
+	const allRoutes = RouteV2.createAllRoutes(allPairs, inputToken, outputToken);
 
-  const { 
-    data: swapHash, 
-    writeContract: writeSwap,
-    error: swapError 
-  } = useWriteContract();
+	const publicClient = createPublicClient({
+		chain: avalancheFuji,
+		key: address,
+		transport: http(),
+	});
 
-  const allPairs = PairV2.initPairs(allTokenPairs);
-  const allRoutes = RouteV2.createAllRoutes(allPairs, inputToken, outputToken);
+	function approveToken(token: string, amount: string) {
+		try {
+			writeApproval({
+				address: getTokenAvax(token, CHAIN_ID).address as Address,
+				abi: abiApprouve,
+				functionName: "approve",
+				args: [router, parseUnits(amount, getTokenAvax(token, CHAIN_ID).decimals)],
+			});
+		} catch (error) {
+			console.error("❌ Approve failed:", error);
+		}
+	}
 
-  const publicClient = createPublicClient({
-    chain: avalancheFuji,
-    key: address,
-    transport: http(),
-  });
+	async function executeSwap() {
+		if (!isApprovalSuccess) {
+			console.error("Please approve USDC first");
+			return;
+		}
 
-  async function approveToken() {
-    try {
-      await writeApproval({
-        address: "0xB6076C93701D6a07266c31066B298AeC6dd65c2d",
-        abi: [
-          {
-            name: "approve",
-            type: "function",
-            stateMutability: "nonpayable",
-            inputs: [
-              {
-                name: "spender",
-                type: "address"
-              },
-              {
-                name: "amount",
-                type: "uint256"
-              }
-            ],
-            outputs: [
-              {
-                name: "",
-                type: "bool"
-              }
-            ]
-          }
-        ],
-        functionName: "approve",
-        args: [router, parseUnits('200', 6)],
-      });
-    } catch (error) {
-      console.error("❌ Approve failed:", error);
-    }
-  }
+		try {
+			const trades = await TradeV2.getTradesExactIn(
+				allRoutes,
+				amountIn,
+				outputToken,
+				false,
+				true,
+				publicClient,
+				CHAIN_ID
+			);
+			console.log(trades);
 
-  async function executeSwap() {
-    if (!isApprovalSuccess) {
-      console.error("Please approve USDC first");
-      return;
-    }
+			const validTrades = trades.filter((trade): trade is TradeV2 => trade !== undefined);
+			const bestTrade = TradeV2.chooseBestTrade(validTrades, isExactIn);
+			console.log(bestTrade);
 
-    try {
-      const trades = await TradeV2.getTradesExactIn(
-        allRoutes,
-        amountIn,
-        outputToken,
-        false,
-        true,
-        publicClient,
-        CHAIN_ID
-      );
+			if (bestTrade) {
+				const { totalFeePct, feeAmountIn } = await bestTrade.getTradeFee();
+				console.log("Total fees percentage", totalFeePct.toSignificant(6), "%");
+				console.log(`Fee: ${feeAmountIn.toSignificant(6)} ${feeAmountIn.token.symbol}`);
 
-      const validTrades = trades.filter((trade): trade is TradeV2 => trade !== undefined);
-      const bestTrade = TradeV2.chooseBestTrade(validTrades, isExactIn);
+				const userSlippageTolerance = new Percent("200", "10000");
+				const swapOptions: TradeOptions = {
+					allowedSlippage: userSlippageTolerance,
+					ttl: 3600,
+					recipient: address as string,
+					feeOnTransfer: false,
+				};
 
-      if (bestTrade) {
-        const { totalFeePct, feeAmountIn } = await bestTrade.getTradeFee();
-        console.log("Total fees percentage", totalFeePct.toSignificant(6), "%");
-        console.log(`Fee: ${feeAmountIn.toSignificant(6)} ${feeAmountIn.token.symbol}`);
+				const {
+					methodName,
+					args,
+					value,
+				} = bestTrade.swapCallParameters(swapOptions);
 
-        const userSlippageTolerance = new Percent("200", "10000");
-        const swapOptions: TradeOptions = {
-          allowedSlippage: userSlippageTolerance,
-          ttl: 3600,
-          recipient: address as string,
-          feeOnTransfer: false,
-        };
+				writeSwap({
+					address: router,
+					abi: LBRouterV22ABI,
+					functionName: methodName,
+					args: args,
+					account: address,
+				});
+			}
+		} catch (error) {
+			console.error("Error executing swap:", error);
+		}
+	}
 
-        const {
-          methodName,
-          args,
-          value,
-        } = bestTrade.swapCallParameters(swapOptions);
+	const word = ["USDC", "USDT", "WAVAX"];
 
-        await writeSwap({
-          address: router,
-          abi: LBRouterV22ABI,
-          functionName: methodName,
-          args: args,
-          account: address,
-        });
-      }
-    } catch (error) {
-      console.error("Error executing swap:", error);
-    }
-  }
+	const handleSelect1Change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedOption1(e.target.value);
+	};
 
-  return (
-    <main className="flex h-screen flex-col items-center gap-4">
-      <h1>Trader Joe Swap</h1>
-      <button 
-        onClick={approveToken}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-        disabled={isApprovalSuccess}
-      >
-        {isApprovalSuccess ? "Approved!" : "Approve USDC"}
-      </button>
-      <button 
-        onClick={executeSwap}
-        className="px-4 py-2 bg-green-500 text-white rounded"
-        disabled={!isApprovalSuccess}
-      >
-        Swap
-      </button>
-      {(approvalError || swapError) && (
-        <div className="text-red-500">
-          {approvalError?.message || swapError?.message}
-        </div>
-      )}
-      {(approvalHash || swapHash) && (
-        <div className="text-green-500">
-          Transaction Hash: {approvalHash || swapHash}
-        </div>
-      )}
-    </main>
-  );
+	const handleSelect2Change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedOption2(e.target.value);
+	};
+	return (
+		<main className="flex h-screen flex-col items-center gap-4">
+			<textarea id="amount" onChange={(e) => setAmount(e.target.value)} placeholder="Amount"></textarea>
+			<select id="select1" onChange={handleSelect1Change} value={selectedOption1 || ""}>
+				<option value="" disabled>Select an option</option>
+				{word.filter(opt => opt !== selectedOption2).map((opt, index) => (
+					<option key={index} value={opt}>{opt}</option>
+				))}
+			</select>
+			<select id="select2" onChange={handleSelect2Change} value={selectedOption2 || ""}>
+				<option value="" disabled>Select an option</option>
+				{word.filter(opt => opt !== selectedOption1).map((opt, index) => (
+					<option key={index} value={opt}>{opt}</option>
+				))}
+			</select>
+			<h1>Trader Joe Swap</h1>
+			<button
+				onClick={() => approveToken(selectedOption1 || "", amount)}
+				className="px-4 py-2 bg-blue-500 text-white rounded"
+				disabled={isApprovalSuccess}
+			>
+				{isApprovalSuccess ? "Approved!" : "Approve USDC"}
+			</button>
+			<button
+				onClick={executeSwap}
+				className="px-4 py-2 bg-green-500 text-white rounded"
+				disabled={!isApprovalSuccess}
+			>
+				Swap
+			</button>
+			{(approvalError || swapError) && (
+				<div className="text-red-500">
+					{approvalError?.message || swapError?.message}
+				</div>
+			)}
+			{(approvalHash || swapHash) && (
+				<div className="text-green-500">
+					Transaction Hash: {approvalHash || swapHash}
+				</div>
+			)}
+		</main>
+	);
 }
